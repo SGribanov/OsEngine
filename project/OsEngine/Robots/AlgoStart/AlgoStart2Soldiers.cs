7754eae341ee48093f4acfaa4ebcb53f8b5f8ae8
Stanislav Gribanov <gsv777@yandex.ru>
2026-02-17 11:03:28 +0500

    refactor(stage2): centralize AlgoStart2Soldiers settings path

--- project/OsEngine/Robots/AlgoStart/AlgoStart2Soldiers.cs
+++ project/OsEngine/Robots/AlgoStart/AlgoStart2Soldiers.cs
@@ -142,7 +142,7 @@ namespace OsEngine.Robots.AlgoStart
         {
             try
             {
-                using (StreamWriter writer = new StreamWriter(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt", false)
+                using (StreamWriter writer = new StreamWriter(GetTradeSettingsPath(), false)
                     )
                 {
                     for (int i = 0; i < _tradeSettings.Count; i++)
@@ -162,13 +162,13 @@ namespace OsEngine.Robots.AlgoStart
         // Load settins from .txt file
         private void LoadTradeSettings()
         {
-            if (!File.Exists(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt"))
+            if (!File.Exists(GetTradeSettingsPath()))
             {
                 return;
             }
             try
             {
-                using (StreamReader reader = new StreamReader(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt"))
+                using (StreamReader reader = new StreamReader(GetTradeSettingsPath()))
                 {
                     while (reader.EndOfStream == false)
                     {
@@ -200,15 +200,20 @@ namespace OsEngine.Robots.AlgoStart
             {
                 _tradePeriodsSettings.Delete();
 
-                if (File.Exists(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt"))
+                if (File.Exists(GetTradeSettingsPath()))
                 {
-                    File.Delete(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt");
+                    File.Delete(GetTradeSettingsPath());
                 }
             }
             catch (Exception)
             {
                 // ignore
             }
+        }
+
+        private string GetTradeSettingsPath()
+        {
+            return @"Engine\" + NameStrategyUniq + @"SettingsBot.txt";
         }
 
         private void AdaptSoldiersHeight(List<Candle> candles, SecuritiesTradeSettings settings)
